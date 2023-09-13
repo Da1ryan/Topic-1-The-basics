be5f2ddcd8cef15bb3e1ea4273bb284831ed1396
339571119 <339571119@2260I-L29-010>
2023-09-13 10:29:18 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_1_The_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
